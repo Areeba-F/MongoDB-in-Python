5d798b666ce3d589dd5a284ddf907a58e34c1ea5
MuYan Zhang <76665125+MuYan-Zhang@users.noreply.github.com>
2022-11-23 09:21:23 -0700

    - Queries are working but indexing and creating new column is slow
    Phase one too slow, indexing and new column takes too long.
    
    This causes articles searches for common keywords take really long too

--- load-json.py
+++ load-json.py
@@ -1,22 +1,62 @@
 import json
+import timeit
+import os
 from pymongo import MongoClient
 from pymongo import TEXT
 
-# TODO: actually make this a user input
-client = MongoClient('mongodb://localhost:27016')
-
-db = client["291db"]
-
-table = db["dblp"]
-
-# load file line by line
-with open('dblp-ref-1m.json') as file:
+"""
+with open('dblp-ref-10.json') as file:
   for obj in file:
-    file_data = json.loads(obj)
-    table.insert_one(file_data)
+    file_data = json.loads(obj) 
+    articles_coll.insert_one(file_data)
+"""
 
-# index
-db.table.create_index( [("$**", TEXT)])
-db.table.create_index("year")
+def main():
+  # Input parameters
+  #input_file = input("Enter input file name/path: ")
+  port_num = input("Enter the port number the MongoDB server is running: ")
+  
+  # Initialize DB
+  client = MongoClient("mongodb://localhost:{}".format(port_num))
+  db = client["291db"]
 
-print("done")
+  #collist = db.list_collection_names()
+  #if "dblp" in collist:
+    #db.dblp.drop() #or maybe articles_coll.delete_many({})
+  dblp = db["dblp"]
+  db.dblp.drop_indexes() #-- should not be necessary anymore, unless testing
+
+  #json mongoimport
+  #command = r'"D:\Program Files\MongoDB\Tools\100\bin\mongoimport.exe" --port={} --db=291db --collection=dblp --file={}'.format(port_num, input_file)
+  #os.system(command)
+  
+  # Inserts a new field: the $year field converted to string
+  new_column_st = timeit.default_timer()
+  dblp.update_many(
+  { },
+  [
+    {"$set": {"str_year1": { "$toString": "$year" }}}
+  ]
+  )
+  new_column_elapsed = timeit.default_timer() - new_column_st
+
+  #dblp.create_index([("$**", TEXT)], default_language = "none") # Now text index will also apply to the str_year field
+  text_index_st = timeit.default_timer()
+  dblp.create_index([("title", TEXT), ("authors", TEXT), ("abstract", TEXT), ("venue", TEXT), ("str_year", TEXT)], default_language = "none")
+  text_index_elapsed = timeit.default_timer() - text_index_st
+  
+  id_index_st = timeit.default_timer()
+  dblp.create_index("id")
+  id_index_elapsed = timeit.default_timer() - id_index_st
+  
+  #debug
+  print(list(db.dblp.index_information()))
+  print("new_column_elapsed: ")
+  print(new_column_elapsed)
+  print("text_index_elapsed: ")
+  print(text_index_elapsed)
+  print("id_index_elapsed: ")
+  print(id_index_elapsed)
+
+if __name__ == "__main__":
+  main()
--- proj2.py
+++ proj2.py
@@ -2,33 +2,41 @@ import json
 from pymongo import MongoClient
 from pymongo import TEXT
 
-# Beginning of Phase 2. Don't know how to use pre-existing DBs so I made a new one again
-
+# Phase one stuff
+"""
 client = MongoClient('mongodb://localhost:27017')
 
 db = client["proj2"]
 
 # Create a collection called "Table", remove anything already inside and all existing indexes, if any 
-articles_coll = db["articles_coll"]
-articles_coll.delete_many({})
-db.articles_coll.drop_indexes()
+dblp = db["dblp"]
+dblp.delete_many({})
+db.dblp.drop_indexes()
 
 with open('dblp-ref-10.json') as file:
   for obj in file:
     file_data = json.loads(obj) 
-    articles_coll.insert_one(file_data)
+    dblp.insert_one(file_data)
 
 # Inserts a new field: the $year field converted to string
-articles_coll.update_many(
+dblp.update_many(
   { },
   [
     {"$set": {"str_year": { "$toString": "$year" }}}
   ]
 )
-#articles_coll.create_index([("$**", TEXT)], default_language = "none") # Now text index will also apply to the str_year field
-articles_coll.create_index([("title", TEXT), ("authors", TEXT), ("abstract", TEXT), ("venue", TEXT), ("year", TEXT)], default_language = "none")
-articles_coll.create_index("id")
-print(list(db.articles_coll.index_information()))
+#dblp.create_index([("$**", TEXT)], default_language = "none") # Now text index will also apply to the str_year field
+dblp.create_index([("title", TEXT), ("authors", TEXT), ("abstract", TEXT), ("venue", TEXT), ("str_year", TEXT)], default_language = "none")
+dblp.create_index("id")
+print(list(db.dblp.index_information()))
+"""
+# Beginning of Phase 2
+port_num = input("Enter the port number the MongoDB server is running: ")
+client = MongoClient("mongodb://localhost:{}".format(port_num))
+db = client["291db"]
+collist = db.list_collection_names()
+if "dblp" in collist:
+  dblp = db["dblp"]
 
 ### 1 - functionality: search for articles
 def search_article(keywords):
@@ -40,90 +48,129 @@ def search_article(keywords):
 
   print(keyword_str)
   
-  results = db.articles_coll.find({"$text": {"$search": "{}".format(keyword_str)}})
+  results = db.dblp.find({"$text": {"$search": "{}".format(keyword_str)}})
   return results
 
 def article_display_more_info(article):
-  print("\n")
+  print("")
   for key in article.keys():
     if(key == "references"):
       # Query
-      refs = list(articles_coll.find({"id": {"$in" : article[key]}})) #is this using the index correctly?
+      refs = list(dblp.find({"id": {"$in" : article[key]}})) #is this using the index correctly?
       # Output references
-      ref_num = 1
       for i in range(0, len(refs)):
-        print("\t---Reference #" + str(ref_num) + "---")
+        print("\t---Reference #" + str(i + 1) + "---")
         print("\tid: " + refs[i]["id"])
         print("\ttitle: " + refs[i]["title"])
         print("\tyear: " + str(refs[i]["year"]) + "\n")
-        ref_num += 1
     else:
       print(str(key) + ": " + str(article[key]))
+  print("")
 
 def search_article_menu():
   keywords = input("Search for articles based on keyword(s): ").split()
   matches = list(search_article(keywords))
-  #matches = search_article(keywords)
-  #matches1 = search_article(keywords) #a separate cursor works, but seems hacky and may not be reliable...
-  #print(type(matches[0]))
-  #print(matches[0])
-  result_num = 1
 
-  # Search summary
+  # Search summary FIX ME, IMPLEMENT PRINT SUMMARY FUNCTION
   for i in range(0, len(matches)):
-    print("\n---" + str(result_num) + "---")
+    print("\n---" + str(i + 1) + "---")
     print("id: " + matches[i]["id"])
     print("title: " + matches[i]["title"])
     print("year: " + str(matches[i]["year"]))
     print("venue: " + matches[i]["venue"])
-    result_num += 1
-  
-  """
-  for art in matches:
-    print("\n---" + str(result_num) + "---")
-    print("id: " + art["id"])
-    print("title: " + art["title"])
-    print("year: " + str(art["year"]))
-    print("venue: " + art["venue"])
-    result_num += 1
-  """
 
   # More info
   article_selection = int(input("Enter an article's number for more info, or \"0\" to return: "))
   if(article_selection == 0):
     return
-  #print(matches.next())
-  #for art in matches: 1
-  #matches1.skip(article_selection-1) #doesn't work on a cursor that has already iterated
-  #print(matches1.next())
-  article_display_more_info(matches[int(article_selection) - 1])
+  article_display_more_info(matches[article_selection - 1])
 
 ### 2 - functionality: search for authors
 def search_authors(keyword):
-  """
+  #FIX ME, compare execution time for using $text index $and regex search in authors field vs. just regex searching in authors
   pipeline = [
+    {
+      "$match":
+        {
+          "$and":[
+            {"$text": {"$search": "{}".format(keyword)}},
+            {"authors": {"$regex" : "{}".format(keyword), "$options" : "i"}}
+          ]
+        }
+    },
+    {
+      "$unwind": "$authors"
+    },
+    {
+      "$match":
+        {
+          "authors": {"$regex" : "{}".format(keyword), "$options" : "i"}
+        }
+    },
     {
       "$group":
         {
-          "_id": "authors",
-          "count_": {}
+          "_id": "$authors",
+          "count_publications": {"$sum": 1}
         }
     }
   ]
-  """
-  results = db.articles_coll.find({"$text": {"$search": "{}".format(keyword)}})
-  total_publications = 0
-  print("search for author...")
+  
+  author_results = db.dblp.aggregate(pipeline)
+  return author_results
+
+def author_display_publications(author):
+  pipeline = [
+    {
+      "$match":
+        {
+          "$and":[
+            {"$text": {"$search": "{}".format(author["_id"])}},
+            {"authors": {"$regex" : "{}".format(author["_id"]), "$options" : "i"}}
+          ]
+        }
+    },
+    {
+      "$sort": {"year": -1}
+    },
+    {
+      "$project": {"_id": 0, "title": 1, "year": 1, "venue": 1}
+    }
+  ]
+  author_publications = db.dblp.aggregate(pipeline)
+
+  #display output, FIX me, implement separate functions
+  for article in author_publications:
+    print("")
+    print("title: " + article["title"])
+    print("year: " + str(article["year"]))
+    print("venue: " + article["venue"])
+  print("")
 
 def search_authors_menu():
   keyword = input("Enter one keyword to search for authors: ")
-  results = search_authors(keyword)
+  author_matches = list(search_authors(keyword))
+  #FIX ME, IMPLEMENT PRINT SUMMARY FUNCTION
+  #Search results summary
+  for i in range(0, len(author_matches)):
+    print("\n---" + str(i + 1) + "---")
+    print("author name: " + author_matches[i]["_id"])
+    print("number of publications: " + str(author_matches[i]["count_publications"]))
+  
+  author_selection = int(input("Enter an author's number to see their publications, or \"0\" to return: "))
+  if(author_selection == 0):
+    return
+  author_display_publications(author_matches[author_selection - 1])
 
 def main():
-  print("---Main Menu---\n1. Search for Articles")
-  main_menu_selection = input("Select an Option: ")
-  if(main_menu_selection == "1"):
-    search_article_menu()
-
+  while(True):
+    print("---Main Menu---\n1. Search for Articles")
+    main_menu_selection = input("Select an Option: ")
+    if(main_menu_selection == "1"):
+      search_article_menu()
+    if(main_menu_selection == "2"):
+      #under development...
+      search_authors_menu()
+  
 if __name__ == "__main__":
   main()
